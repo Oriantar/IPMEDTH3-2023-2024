6b6046d1e7a163acec8464923fc17987f2eb0965
diewert <s1136860@student.hsleiden.nl>
2024-01-15 13:19:51 +0100

    vleesBakken functie gemaakt en aangeroepen in de place.
    vleesbakken functie is aangemaakt, en toegepast in de place functie.

--- BBQ/JS/index.js
+++ BBQ/JS/index.js
@@ -5,7 +5,7 @@ window.onload = () =>{
     const camera = document.getElementById('js--camera');
     let scene = document.getElementById('js--scene')
     let hold = null;
-    var timer;
+    var tijd;
     
 
 
@@ -27,19 +27,46 @@ window.onload = () =>{
         }
     }
     //timer maken
-    (function(){
-        var sec = 0;
-        timer = setInterval(()=>{
+    function timer(sec){
+        tijd = setInterval(()=>{
+            var sec = 0;
             sec ++;
             console.log(sec);
         }, 1000) // elke seconde
-    })()
+    }
+
+    function vleesBakken(positie, box){
+        if (positie == 0){
+            (function(){
+                var sec = 0;
+                timer = setInterval(()=>{
+                    sec ++;
+                    console.log(sec);
+                    if(sec == 5){
+                        box.setAttribute('color', 'brown');
+                        active = false;
+    
+                    }
+                    if(sec == 10){
+                        box.setAttribute('color', 'black');
+                        active = false;
+                    }
+
+                }, 1000) // elke seconde
+                
+            })()
+            
+        }
+
+
+    }
 
 
     place = (id, object) => {
         let box = document.createElement('a-box');
         let nodeMap = document.getElementById(id).attributes;
         let pos = object.getAttribute('position');
+        console.log(pos.z);
         box.setAttribute('position', {x: pos.x, y:"0.2", z: pos.z});
         box.setAttribute('class', 'js--clickable js--pickup');
         for (let i = 0; i < nodeMap.length; i++){
@@ -51,12 +78,13 @@ window.onload = () =>{
 
         }
 
-
+    
         scene.appendChild(box);
+        vleesBakken(pos.z, box); //vlees wordt bruin/zwart na bepaalde tijd als het op de goede locatie geplaatst is.
         document.getElementsByClassName("js--hold").item(0).remove();
         hold = null;
         addListeners();
-
+        
     }
     pickup = (id, camera) => {
             let nodeMap = document.getElementById(id).attributes;
